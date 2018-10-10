3516b5506d5fb308ca67af3641713f7e26733ea8
MercuryOne <471565939@qq.com>
2018-10-10 23:24:59 +0800

    二分算法测试

--- demo-test/src/test/java/com/test/demotest/DemoTestApplicationTests.java
+++ demo-test/src/test/java/com/test/demotest/DemoTestApplicationTests.java
@@ -19,4 +19,34 @@ public class DemoTestApplicationTests {
         System.err.println("test== ");
     }
 
+    /* 二分查询算法 */
+    @Test
+    public void binarySelectTest() {
+        int[] arr = {0,3,6,9,11,25,36,100,1000};
+        int start = 0;
+        int end = arr.length;
+        int data = 36;
+        Object o = binarySelect(arr, start, end, data);
+        System.err.println("===" + o);
+
+    }
+
+    private Object binarySelect(int[] arr,int start,int end,int data) {
+
+        int mid = (start + end) >> 1;
+        if (arr[mid] == data) {
+            return arr[mid];
+        }
+        if (arr[mid] < data) {
+            start = mid + 1;
+            return binarySelect(arr,start,end,data);
+        } else if (arr[mid] > data){
+            end = mid -1;
+            return binarySelect(arr,start,end,data);
+        }
+        return -1;
+    }
+
+
+
 }
